97decf0cd7b5e949e4bc00ff6e62c899cadc65d5
Rakkan  Tiasakul <rakkantia@gmail.com>
2017-09-17 17:30:14 +0700

    Update 5810546013-client.py

--- 5810546013-client.py
+++ 5810546013-client.py
@@ -5,7 +5,7 @@ server_check = True
 
 while(server_check):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('10.2.21.75',12345)) 
+    s.connect(('127.0.0.1',12345)) 
     # stringTosend = (str(input()))
     # s.send(stringTosend.encode())
     s.send('{}\r\n'.format(input()).encode())
